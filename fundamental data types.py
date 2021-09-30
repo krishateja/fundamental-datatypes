ac4a07119f60dd2a4e210206769a4f3d91308bc3
krishnateja.Moturi <87372676+krishateja@users.noreply.github.com>
2021-09-30 21:40:19 +0530

    Add files via upload

---
+++ fundamental data types.py
@@ -0,0 +1,144 @@
+a=100
+print(bin(a))
+b=0b1100100
+print(int(b))
+c=753
+print(c)
+print(bin(c))
+print(oct(c))
+d=0b1011110001
+print(int(d))
+print(oct(d))
+e=0o1361
+print(int(e))
+print(bin(e))
+f=777
+print(f)
+print(bin(f))
+print(oct(f))
+print(hex(f))
+g=0b1100001001
+print(int(g))
+print(oct(g))
+print(hex(g))
+h=0o1411
+print(int(h))
+print(bin(h))
+print(hex(h))
+i=0x309
+print(int(i))
+print(bin(i))
+print(oct(i))
+print(float(a))
+print(float(True))
+print(float("73.52"))
+j=0xface
+print(j)
+print(int(j))
+print(bin(j))
+print(oct(j))
+import keyword
+print(keyword.kwlist)
+print(True)
+print(None)
+print(int(True))
+print(int(False))
+print(bool(1))
+print(bool(0))
+h=34.6
+print(int(h))
+print(bool(h))
+k=34
+print(float(k))
+print(bool(h))
+l=True
+print(int(l))
+print(float(l))
+m=10+7j
+print(bool(m))
+print(bool("True"))
+print(bool("0"))
+print(bool(0))
+print(bool(""))
+n="100"
+print(n)
+print(type(n))
+#complex
+o=10+2j
+print(o)
+print(bool(o))
+print(str(o))
+print(type(o))
+print(o.real)
+print(o.imag)
+print(complex(9,2))
+print(complex(8))
+print(complex(0,3))
+#print(complex(8,0))
+print(complex(12.25))
+print(complex(7.7,8.8))
+print(complex(True))
+print(complex(False))
+print(complex(False,True))
+print(complex(4+5j,3+6j))
+print(complex(7.7))
+#string
+p="100200"
+print(p)
+print(int(p))
+print(float(p))
+print(complex(p))
+print(bool(p))
+print(str(p))
+list1=["raju","teja","chandu"]
+del list1[2]
+print(list1)
+print(list1[1])
+print(list1)
+list1.clear()
+print(list1)
+del list1
+x="hii guys good night"
+print(len(x))
+print(x[2])
+print(x[-4])
+print(x[len(x)-4])
+print(x[4:8:1])
+print(x[ : :])
+print(x[ :len(x): ])
+print(x[ :len(x)])
+print(x[-7:-11:-1])
+print(x[ : : -1])
+y="welcome"
+print(len(y))
+print(y[6])
+print(y[-2])
+z= """Create a new string object from the given object. If encoding or
+errors is specified, then the object must expose a data buffer
+that will be decoded using the given encoding and error handler"""
+print(len(z))
+print(z[-1:-191:-1])
+print(z[-64: -107: -1])
+print(len("Create a new string object from the given object."))
+print(len(" encoding and error handler"))
+print(z[50:164:1])
+print(z[-27:-142:-1])
+n=""" If encoding or errors is specified, then the object must expose a data buffer that will be decoded using the given """
+print(len(n))
+a="welcome to the world"
+l=a.split()
+print(l)
+print(l[2])
+print(len(l))
+
+
+
+
+
+
+
+
+
+
+
+
